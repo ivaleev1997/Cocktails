51eddf7cdc0cd32e4a4e8e3de9b08695f8fc62ad
Ivan Ivaleev <ivaleev1997@gmail.com>
2020-01-23 22:57:16 +0300

    Start di creation. Added files: AppComponent, AppModule, ViewModelKey, ViewModelModule, CocktailsListFragmentViewModel and AppViewModuleFactory.

---
+++ app/src/main/java/com/education/cocktails/CocktailsListFragmentViewModel.kt
@@ -0,0 +1,6 @@
+package com.education.cocktails
+
+import androidx.lifecycle.ViewModel
+
+class CocktailsListFragmentViewModel(): ViewModel() {
+}
---
+++ app/src/main/java/com/education/cocktails/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.education.cocktails.di
+
+class AppModule {
+}
